2c6c262aad2e85ac97f7285891e037e26ea05b07
Shiv200 <86182486+Shiv200@users.noreply.github.com>
2021-06-27 13:06:11 +0530

    Add files via upload

---
+++ c9-STUD-TEACHER-main/c9-STUD-TEACHER-main/sketch.js
@@ -0,0 +1,22 @@
+
+
+var a = prompt("Enter the first variable:");
+var b = prompt("Enter the second variable:");
+
+function setup(){
+  createCanvas(400,400);
+  var b2 = createButton("click here to Swap numbers");
+  b2.mousePressed(swap);
+  }
+
+
+function draw()
+{
+  background[220];
+}
+
+function swap(){
+  [a,b]=[b,a]
+  console.log("the value of a after swapping"+a);
+  console.log("the value of b after swapping"+b);
+}
